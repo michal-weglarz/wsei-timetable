bf01cad7507fde1028ca5e279c16690955fd927e
Michał Węglarz <michal.weglarz@protonmail.com>
2019-04-09 23:06:01 +0200

    Changed settings loading screen's icon

--- react-native/wsei-timetable/screens/Loading.js
+++ react-native/wsei-timetable/screens/Loading.js
@@ -1,10 +1,15 @@
 import * as React from 'react';
-import { Text, View, ActivityIndicator } from 'react-native';
+import { Text, View, ActivityIndicator, Image } from 'react-native';
 
 export default class LoadingScreen extends React.Component {
 	render() {
 		return (
 			<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+				<Image
+					source={require('../assets/icons/meditation.png')}
+					style={{ width: 125, height: 125 }}
+				/>
+				<Text style={{ marginTop: 15, marginBottom: 15 }}>Trwa wczytywanie planu zajęć...</Text>
 				<ActivityIndicator size="large" color="#DD688F" />
 			</View>
 		);
